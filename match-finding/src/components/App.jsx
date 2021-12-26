9d328d30ead58e8f654712d6d2b255c631183a46
sisackov <1123889+sisackov@users.noreply.github.com>
2021-12-26 09:47:47 +0200

    images fixed

--- match-finding/src/components/App.jsx
+++ match-finding/src/components/App.jsx
@@ -3,13 +3,12 @@ import ButtonComponent from './ButtonComponent';
 import CounterComponent from './CounterComponent';
 import './styles/App.css';
 
-
 const MAX_COUNT = 10;
 
 class App extends React.Component {
-
     constructor(props) {
         super(props);
+        this.images = [];
         this.state = {
             currentIndex: 0,
             approved: 0,
@@ -20,15 +19,15 @@ class App extends React.Component {
         };
     }
 
-    componentDidMount() {
+    async componentDidMount() {
         this.setState({
             imageUrl: this.getImageUrl(this.state.currentIndex),
         });
     }
 
     getImageUrl = (index) => {
-        return `https://picsum.photos/500/500?random=${index}`;
-    }
+        return `https://placeimg.com/640/480/animals/${index}`;
+    };
 
     setImageAndIndex = (index) => {
         this.setState({
@@ -36,7 +35,7 @@ class App extends React.Component {
             imageUrl: this.getImageUrl(index),
             isDone: index >= MAX_COUNT,
         });
-    }
+    };
 
     handleApproveClick = () => {
         let { currentIndex, approved } = this.state;
@@ -46,7 +45,7 @@ class App extends React.Component {
             approved: approved,
         });
         this.setImageAndIndex(currentIndex);
-    }
+    };
 
     handleRejectClick = () => {
         let { currentIndex, rejected } = this.state;
@@ -56,16 +55,18 @@ class App extends React.Component {
             rejected: rejected,
         });
         this.setImageAndIndex(currentIndex);
-    }
+    };
 
     renderDone = () => {
         const { approved, rejected } = this.state;
         return (
             <div className='app-container'>
-                {approved > rejected ? 'You must really like this' : 'You really dont like this'}
+                {approved > rejected
+                    ? 'You must really like this'
+                    : 'You really dont like this'}
             </div>
         );
-    }
+    };
 
     render() {
         const { approved, rejected, imageUrl, isDone } = this.state;
@@ -75,16 +76,27 @@ class App extends React.Component {
             return (
                 <div className='app-container'>
                     <div className='button-row'>
-                        <CounterComponent counter={rejected} label='rejected' color='red' />
-                        <CounterComponent counter={approved} label='approved' color='green' />
+                        <CounterComponent
+                            counter={rejected}
+                            label='rejected'
+                            color='red'
+                        />
+                        <CounterComponent
+                            counter={approved}
+                            label='approved'
+                            color='green'
+                        />
                     </div>
-                    <img
-                        alt='Random Picsum'
-                        src={imageUrl}
-                    />
+                    <img alt='Random Picsum' src={imageUrl} />
                     <div className='button-row'>
-                        <ButtonComponent label='dislike' parentClickHandler={this.handleRejectClick} />
-                        <ButtonComponent label='like' parentClickHandler={this.handleApproveClick} />
+                        <ButtonComponent
+                            label='dislike'
+                            parentClickHandler={this.handleRejectClick}
+                        />
+                        <ButtonComponent
+                            label='like'
+                            parentClickHandler={this.handleApproveClick}
+                        />
                     </div>
                 </div>
             );
